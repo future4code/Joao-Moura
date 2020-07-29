78878010871da10ee94c19fa1f038638d2077f6c
Paulo Moura <joaopaulo.034a@gmail.com>
2020-07-29 16:44:55 -0300

    finalizado os desafios

--- semana3/condicionais/js-vanilla-template/src/index.js
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -95,3 +95,172 @@
     // } else {
     //     console.log("Escolha outro filme :(")
     // }
+
+
+
+//DESAFIOS--------------------
+
+//Desafio 1:
+    // let genero = prompt("Qual gênero de filme você quer assistir?")
+    // let preco = Number(prompt("Qual o o preço do ingresso?"))
+    // let snack = prompt("Qual snack que você quer comprar?")
+
+    // if( genero === "fantasia" && preco < 15){
+    //     console.log("Bom filme!")
+    //     console.log("... com ",snack)
+    // } else {
+    //     console.log("Escolha outro filme :(")
+    // }
+
+
+
+//Desafio 2:
+
+// let nome = prompt("Informe seu nome conpleto:")
+// let tipoDeJogo = prompt("Escolha o tipo de jogo: [IN] para internacional ou [DO] para domestico")
+// let etapaDoJogo = prompt("Escolha a etapa do jogo: [SF] para semi-final, [DT] para decisao de terceiro lugar ou [FI] para final")
+// let categoria = Number(prompt("Escolha a categoria: [1]-[2]-[3]-[4]"))
+// let qtdIngressos = Number(prompt("Digite a quantidade de ingressos: "))
+
+// let ingressoUnd = 0
+
+
+// console.log("---Dados da compra---")
+// console.log("Nome do cliente: ",nome)
+
+
+// //========= CASO O JOGO SEJA INTERNACIONAL ========
+// if (tipoDeJogo === "IN"){
+//     console.log("Tipo do jogo: Internacional")
+
+//     if(etapaDoJogo === "SF"){
+//         console.log("Etapa do jogo: Semifinais")
+
+//         switch(categoria){
+//             case 1:
+//                 ingressoUnd = 1320 * 0.24390244
+//                 break
+//             case 2:
+//                 ingressoUnd = 880 * 0.24390244
+//                 break
+//             case 3:
+//                 ingressoUnd = 550 * 0.24390244
+//                 break
+//             case 4:
+//                 ingressoUnd = 220 * 0.24390244
+//                 break
+//         }   
+
+//     } else if (etapaDoJogo === "DT"){
+//         console.log("Etapa do jogo: Decisão do 3°lugar")
+
+//         switch(categoria){
+//             case 1:
+//                 ingressoUnd = 660 * 0.24390244
+//                 break
+//             case 2:
+//                 ingressoUnd = 440 * 0.24390244
+//                 break
+//             case 3:
+//                 ingressoUnd = 330 * 0.24390244
+//                 break
+//             case 4:
+//                 ingressoUnd = 170 * 0.24390244
+//                 break
+//         }   
+
+//     } else if (etapaDoJogo === "FI"){
+//         console.log("Etapa do jogo: final")
+
+//         switch(categoria){
+//             case 1:
+//                 ingressoUnd = 1980 * 0.24390244
+//                 break
+//             case 2:
+//                 ingressoUnd = 1320 * 0.24390244
+//                 break
+//             case 3:
+//                 ingressoUnd = 880 * 0.24390244
+//                 break
+//             case 4:
+//                 ingressoUnd = 330 * 0.24390244
+//                 break
+//         }   
+//     }
+
+//     console.log("Categoria: ", categoria)
+//     console.log("Quatidade de Ingresso: "+ qtdIngressos +"ingressos")
+//     console.log("---valores---")
+//     console.log("Valor do ingresso: US"+ ingressoUnd)
+//     console.log("Valor total: U$"+ ingressoUnd*qtdIngressos)
+
+
+
+
+// //======= CASO O JOGO SEJA NACIONAL ==========
+// } else if (tipoDeJogo === "DO"){
+//     console.log("Tipo do jogo: Nacional")
+//     if(etapaDoJogo === "SF"){
+//         console.log("Etapa do jogo: Semifinais")
+
+//         switch(categoria){
+//             case 1:
+//                 ingressoUnd = 1320
+//                 break
+//             case 2:
+//                 ingressoUnd = 880 
+//                 break
+//             case 3:
+//                 ingressoUnd = 550
+//                 break
+//             case 4:
+//                 ingressoUnd = 220 
+//                 break
+//         }   
+//     } else if (etapaDoJogo === "DT"){
+//         console.log("Etapa do jogo: Decisão do 3°lugar")
+//         switch(categoria){
+//             case 1:
+//                 ingressoUnd = 660 
+//                 break
+//             case 2:
+//                 ingressoUnd = 440 
+//                 break
+//             case 3:
+//                 ingressoUnd = 330
+//                 break
+//             case 4:
+//                 ingressoUnd = 170 
+//                 break
+//         }   
+//     } else if (etapaDoJogo === "FI"){
+//         console.log("Etapa do jogo: final")
+//         switch(categoria){
+//             case 1:
+//                 ingressoUnd = 1980 
+//                 break
+//             case 2:
+//                 ingressoUnd = 1320 
+//                 break
+//             case 3:
+//                 ingressoUnd = 880 
+//                 break
+//             case 4:
+//                 ingressoUnd = 330 
+//                 break
+//         }   
+//     }
+
+//     console.log("Categoria: ", categoria)
+//     console.log("Quatidade de Ingresso: "+ qtdIngressos +" ingressos")
+//     console.log("---valores---")
+//     console.log("Valor do ingresso: R$"+ ingressoUnd)
+//     console.log("Valor total: R$"+ ingressoUnd*qtdIngressos)
+
+// }
+
+
+
+
+
+
