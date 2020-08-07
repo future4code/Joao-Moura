e0c3c8c0f4c0ed7f5d74b3b0c16f27e55c95927f
Paulo Moura <joaopaulo.034a@gmail.com>
2020-08-06 22:46:29 -0300

    atividade  controlador de despesas finalizada sem desafio

--- semana4/funcoes-de-array/controlador-de-despesas/script.js
+++ semana4/funcoes-de-array/controlador-de-despesas/script.js
@@ -1,7 +1,8 @@
-let arrayDespesas = []
+let arrayDespesas = [] // ARRAY PRINCIPAL
 
+
+//EXIBI A LISTAGEM DE DESPESAS
 function exibirDados(arrayDespesas){
-    console.table(arrayDespesas)// remover
     const lista = document.getElementById('lista')
     lista.innerHTML=''
 
@@ -10,12 +11,13 @@ function exibirDados(arrayDespesas){
             <li>
                 <h3>${item.tipo}</h4>
                 <p>${item.descricao}</p>
-                <p>R$${item.valor},00</p>
+                <p>R$${item.valor}</p>
             </li>`
 
     }
 }
 
+//LIMPA OS CAMPOS DO FORMULARIO DE CADASTRO E DE FILTRAGEM
 function limparCampos(id){
     if(id === "bnt-cadastrar"){
         document.getElementById('valor').value =''
@@ -27,9 +29,20 @@ function limparCampos(id){
     
 }
 
+//EXIBI O VALOR TOTAL DOS GASTOS
+function exibirExtrato(arrayDespesas){
+    let total = 0
+    for(let item of arrayDespesas){
+        total += item.valor
+    }
+    const extrato = document.getElementById('total')
+    extrato.innerHTML = `<h1>R$ ${total}</h1>`
+}
+
+//BUSCA OS DADOS DO FORMULARIO DE CADASTRO E ADICIONA OS OBJETOS AO ARRAY
 function dadosCadastro(evento){ 
     evento.preventDefault()
-    console.log()
+   
 
     const valor = document.getElementById('valor').value
     const tipoDespesa = document.getElementById('despesa').value
@@ -37,7 +50,7 @@ function dadosCadastro(evento){
 
     if(valor !== '' && tipoDespesa !== '' && descricao !==''){
         const despesa = {
-            valor: valor,
+            valor: Number(valor),
             tipo: tipoDespesa,
             descricao: descricao
         }
@@ -45,29 +58,48 @@ function dadosCadastro(evento){
         
         limparCampos(evento.target.id)
         exibirDados(arrayDespesas)
+        exibirExtrato(arrayDespesas)
 
     } else {
         alert('Campos vazios')
     }
    
-    
 }
 
-
+//ENVIA OS CAMPOS DO FORMULARIO FILTRO PARA SEREM LIMPOS #linha 20
 function limparFiltros(evento){
     evento.preventDefault()
     limparCampos(evento.target.id)
 
 }
 
+//BUSCA OS DADOS DO FORMULARIO FILTRO E REALIZA A FILTRAGEM
 function dadosFiltro(evento){
     evento.preventDefault()
 
-    document.getElementById('filtro-despesa').value
-    document.getElementById('valor-min').value 
-    document.getElementById('valor-max').value 
+    const tipoDespesa = document.getElementById('filtro-despesa').value
+    const valorMin = Number(document.getElementById('valor-min').value) 
+    const valorMax = Number(document.getElementById('valor-max').value) 
 
 
+    if( valorMin !== '' && valorMax !=='' ){
 
+        if(arrayDespesas.length !== 0){
+            const arrayFiltrado = arrayDespesas.filter((item)=>{
+                if(item.valor >= valorMin && item.valor <= valorMax && item.tipo === tipoDespesa){
+                    return true
+                }
+            })
+            
+            limparCampos(evento.target.id)
+            exibirDados(arrayFiltrado)
+
+        } else {
+            alert('Nenhuma despesa encontrada!')
+        }
+
+    } else {
+        alert('Campos vazios')
+    }
 }
 
