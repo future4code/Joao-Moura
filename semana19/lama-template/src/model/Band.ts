cd7ec17bd3cbdeed97c290bcf8f1fc145cd847b5
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-11-27 10:19:41 -0300

    feat(model): criando classe para a entidade Banda

---
+++ semana19/lama-template/src/model/Band.ts
@@ -0,0 +1,37 @@
+export class Band{
+    constructor(
+        private id: string,
+        private name: string,
+        private musicGenre: string,
+        private responsible: string
+    ){}
+
+    public getID = () => this.id
+    public getName = () => this.name
+    public getMusicGenre = () => this.musicGenre
+    public getResponsible = () => this.responsible
+
+    public setID = (id: string):void => {
+        this.id = id
+    }
+    public setName = (name: string):void => {
+        this.name = name
+    }
+    public setMusicGenre = (musicGenre: string):void => {
+        this.musicGenre = musicGenre
+    }
+    public setResponsible = (responsible: string):void => {
+        this.responsible = responsible
+    }
+
+    static toBandModel (band: any): Band {
+        return new Band(band.id, band.name, band.musicGenre, band.responsible)
+    }
+
+}
+
+export interface BandInputDTO {
+    name: string,
+    musicGenre: string,
+    responsible: string
+}
